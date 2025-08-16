95802cfe28866f97e3f59c17fae4c9809d175e17
ShwetaSubhashRokade <rokades310@gmail.com>
2025-08-16 16:17:32 +0530

    added subtract feature

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,9 +1,6 @@
 class Calculator
 {
-    public int add(int a , int b)
-    {
-        return a + b ;
-    }
+    
 
     public int sub(int a , int b)
     {
@@ -18,7 +15,7 @@ public class CalculatorTest
     public static void main(String[] args) 
     {
         Calculator  calc = new Calculator();
-        System.out.println("Result:" + calc.add(10, 20));
+        
 
         System.out.println("Result:" + calc.sub(10, 20));
         
